db2038fb8fdecd428f2114988e6fd77f4eab80f1
Nigel Gordon <nigelgordon@Nigels-MacBook-Pro.local>
2017-03-31 20:48:58 +0800

    Added DELETE api call

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
+
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -66,6 +68,38 @@ app.delete('/todos/:id', (req, res) => {
         res.status(400).send();
     });
 });
+
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    // decide which properties sent by user in body you want to update
+    var body = _.pick(req.body, ['text', 'completed'])
+
+    // if body.completed is a boolean and the boolean is true:
+    if (_.isBoolean(body.completed) && body.completed) {
+        // we set the completedAt field (not the user)
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if(!todo) {
+            return res.status(400).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    })
+});
+
+
 
 
 app.listen(port, () => {
