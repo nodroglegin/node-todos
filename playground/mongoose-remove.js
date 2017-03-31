dbc07efc92551376e0635e30d2fc14da72539e7a
Nigel Gordon <nigelgordon@Nigels-MacBook-Pro.local>
2017-03-31 21:19:23 +0800

    Setting up testing database and env variable

--- playground/mongoose-remove.js
+++ playground/mongoose-remove.js
@@ -4,3 +4,16 @@ const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
 const {User} = require('./../server/models/user');
 
+// remove won't send documents back
+Todo.remove({}).then((result) => {
+    console.log(result);
+});
+
+// findOneAndRemove will send the docuement back that was deleted.
+Todo.findAndRemove({_id: '57nnffasdf'}).then((todo) => {
+    console.log(todo);
+})
+
+Todo.findByIdAndRemove('57nnffasdf').then((todo) => {
+    console.log(todo);
+})
